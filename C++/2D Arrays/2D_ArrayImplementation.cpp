dcb28e620bc3025753a0fde95a7025f8f8c29e5e
Nischit Patel <47689523+nischitpatel@users.noreply.github.com>
2021-07-06 21:53:44 +0530

    Create 2D Array in C++
    Implementation of Two Dimensional array in C++

---
+++ C++/2D Arrays/2D_ArrayImplementation.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int r,c;
+	cout<<"Enter number of rows and colums : ";
+	cin>>r>>c;
+	int arr[r][c];
+	for(int i=0;i<r;i++){
+		for(int j=0;j<c;j++){
+			cin>>arr[i][j];
+		}
+	}
+	for(int i=0;i<r;i++){
+		for(int j=0;j<c;j++){
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+}
