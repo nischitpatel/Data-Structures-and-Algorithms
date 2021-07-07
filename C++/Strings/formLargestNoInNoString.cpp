eb92f8af0fa79643f823a4dcffcc30e1d92309be
Nischit Patel <47689523+nischitpatel@users.noreply.github.com>
2021-07-07 21:29:56 +0530

    Form largest number in a number string in C++
    Form largest number in number string in C++

---
+++ C++/Strings/formLargestNoInNoString.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	string s="123456";
+	sort(s.begin(), s.end(), greater<int>());
+	cout<<s<<endl;
+	return 0;
+}
